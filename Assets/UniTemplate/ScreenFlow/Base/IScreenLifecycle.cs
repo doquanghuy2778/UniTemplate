664ddcbdd5b7fa05c29d23956c928458ecc8a169
dqhuy <dohuy0079@gmail.com>
2025-08-13 20:11:52 +0700

    add interface manager screen life cycle

---
+++ Assets/UniTemplate/ScreenFlow/Base/IScreenLifecycle.cs
@@ -0,0 +1,32 @@
+﻿namespace UniTemplate.ScreenFlow.Base
+{
+    using Cysharp.Threading.Tasks;
+    using UnityEngine;
+
+    public interface IScreenLifecycle
+    {
+        public string       ScreenId { get; }
+        public ScreenStatus Status   { get; set; }
+        public void         SetViewParent(Transform parent);
+        public Transform    GetViewParent();
+        public UniTask      BindData();
+        public UniTask      OpenViewAsync();
+        public UniTask      CloseViewAsync();
+        public void         CloseView();
+        public void         HideView();
+        public void         DestroyView();
+    }
+    
+    public interface IScreenLifecycle<in TModel> : IScreenLifecycle
+    {
+        public UniTask OpenView(TModel model);
+    }
+
+    public enum ScreenStatus
+    {
+        Opened,
+        Closed,
+        Hide,
+        Destroyed
+    }
+}
