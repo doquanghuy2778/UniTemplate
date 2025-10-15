7e230ef98e723a3571f883410146735657c6c6ce
Do Quang Huy <huydq@admin123.com>
2025-10-15 10:25:20 +0700

    fix template

--- Assets/UniTemplate/ScreenFlow/Manager/ScreenManager.cs
+++ Assets/UniTemplate/ScreenFlow/Manager/ScreenManager.cs
@@ -3,12 +3,10 @@ namespace UniTemplate.ScreenFlow.Manager
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using AssetsManager.AssetsManager.Runtime;
     using Cysharp.Threading.Tasks;
-    using UniTemplate.AssetsManager;
     using UniTemplate.ScreenFlow.Base;
-    using UnityEngine;
     using VContainer.Unity;
-    using Object = System.Object;
 
     public interface IScreenManager
     {
@@ -26,31 +24,31 @@ namespace UniTemplate.ScreenFlow.Manager
         /// </summary>
         /// <returns></returns>
         public UniTask CloseAllScreenAsync();
-        
+
         /// <summary>
         /// Close all screen in queue
         /// </summary>
         public void CloseAllScreen();
 
     }
-    
+
     public class ScreenManager : IScreenManager, ITickable, IInitializable, IDisposable
     {
         #region Constructor
-        
+
         private readonly IGameAssets gameAssets;
 
         private ScreenManager(IGameAssets gameAssets)
         {
             this.gameAssets = gameAssets;
         }
-        
+
         #endregion
-       
+
         private readonly List<IScreenLifecycle>                   activeScreens       = new();
         private readonly Dictionary<Type, IScreenLifecycle>       typeToLoadScreen    = new();
         private readonly Dictionary<Type, Task<IScreenLifecycle>> typeToPendingScreen = new();
-        
+
         // public async UniTask<T> GetScreen<T>() where T : IScreenLifecycle
         // {
         //     var screenType = typeof(T);
@@ -82,42 +80,42 @@ namespace UniTemplate.ScreenFlow.Manager
         //         return (T)screenPresenter;
         //     }
         // }
-        
+
         public UniTask<TPresenter> OpenScreen<TPresenter>() where TPresenter : IScreenLifecycle
         {
             throw new System.NotImplementedException();
         }
-        
+
         public UniTask<TPresenter> OpenScreen<TPresenter, TModel>(TModel model) where TPresenter : IScreenLifecycle<TModel>
         {
             throw new System.NotImplementedException();
         }
-        
+
         public UniTask CloseCurrentScreen()
         {
             throw new System.NotImplementedException();
         }
-        
+
         public UniTask CloseAllScreenAsync()
         {
             throw new System.NotImplementedException();
         }
-        
+
         public void CloseAllScreen()
         {
             throw new System.NotImplementedException();
         }
-        
+
         public void Tick()
         {
             throw new NotImplementedException();
         }
-        
+
         public void Initialize()
         {
             throw new NotImplementedException();
         }
-        
+
         public void Dispose()
         {
             throw new NotImplementedException();
