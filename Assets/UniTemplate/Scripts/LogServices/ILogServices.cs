380f793a2749a761c91d6eaaca42d79a7d73a776
Do Quang Huy <huydq@admin123.com>
2025-10-22 14:13:46 +0700

    update log services

--- Assets/UniTemplate/Scripts/LogServices/ILogServices.cs
+++ Assets/UniTemplate/Scripts/LogServices/ILogServices.cs
@@ -3,6 +3,11 @@
     public interface ILogServices
     {
         public void LogCallerInfor(int line, string memberName, string sourceFilePath);
-        public void LogError(string    message);
+
+        public void LogError(string message);
+
+        public void Log(string message);
+
+        public void LogException(string message);
     }
 }
--- Assets/UniTemplate/Scripts/LogServices/LogServicesManager.cs
+++ Assets/UniTemplate/Scripts/LogServices/LogServicesManager.cs
@@ -5,16 +5,27 @@
     public class LogServicesManager : ILogServices
     {
         public void LogCallerInfor(
-            [System.Runtime.CompilerServices.CallerLineNumber] int line = 0,
+            [System.Runtime.CompilerServices.CallerLineNumber] int    line       = 0,
             [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
-            [System.Runtime.CompilerServices.CallerFilePath] string filePath = "")
+            [System.Runtime.CompilerServices.CallerFilePath]   string filePath   = ""
+        )
         {
             Debug.Log($"{line} :: {memberName} :: {filePath}");
         }
 
         public void LogError(string message)
         {
-            throw new System.NotImplementedException();
+            Debug.LogError(message);
+        }
+
+        public void Log(string message)
+        {
+            Debug.Log(message);
+        }
+
+        public void LogException(string message)
+        {
+            Debug.LogException(new System.Exception(message));
         }
     }
 }
