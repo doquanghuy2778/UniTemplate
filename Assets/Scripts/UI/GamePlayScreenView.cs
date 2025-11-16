9290bceabc31935b45dff88039f87a82ac533ea3
dqhuy <dohuy0079@gmail.com>
2025-11-16 16:40:35 +0700

    add screen demo & update submodule

---
+++ Assets/Scripts/UI/GamePlayScreenView.cs
@@ -0,0 +1,30 @@
+﻿namespace GameFoundationCore.HyperCasual.UI
+{
+    using GameDevelopmentKit.GameFoundationCore.Scripts.ScreenFlow.Base.Presenter;
+    using GameDevelopmentKit.GameFoundationCore.Scripts.ScreenFlow.Base.View;
+    using GameFoundationCore.LogServices;
+    using GameFoundationCore.Signals;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class GamePlayScreenView : BaseView
+    {
+        [field: SerializeField] public Image BackgroundImage { get; set; }
+    }
+
+    [ScreenInfo(nameof(GamePlayScreenView))]
+    public class GamePlayScreenPresenter : BaseScreenPresenter<GamePlayScreenView>
+    {
+        protected GamePlayScreenPresenter(
+            SignalTransmitter signalTransmitter, 
+            ILogServices logServices
+            ) : base(signalTransmitter, logServices)
+        {
+        }
+
+        private void SetBackGround()
+        {
+            this.View.BackgroundImage.color = Color.black;
+        }
+    }
+}
