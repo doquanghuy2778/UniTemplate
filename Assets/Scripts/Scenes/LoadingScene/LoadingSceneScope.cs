75a18827428b28475f808382791fd45dea169381
Do Quang Huy <huydq@admin123.com>
2025-11-18 13:08:00 +0700

    update project

--- Assets/Scripts/Scenes/LoadingScene/LoadingSceneScope.cs
+++ Assets/Scripts/Scenes/LoadingScene/LoadingSceneScope.cs
@@ -1,12 +1,17 @@
 namespace Bow.Scripts.Scenes.LoadingScene
 {
+    using GameDevelopmentKit.GameFoundationCore.Scene;
     using GameFoundationCore.DI;
+    using GameFoundationCore.Scripts.Extension;
+    using UnityEngine;
     using VContainer;
 
     public class LoadingSceneScope : SceneScope
     {
         protected override void Configure(IContainerBuilder builder)
         {
+            Debug.Log("LoadingSceneScope Configure");
+            builder.InitScreenManually<TemplateLoadingScreenPresenter>();
         }
     }
 }
